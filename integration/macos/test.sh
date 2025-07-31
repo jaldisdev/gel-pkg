fa5d7b5d792db0938355ca91e3ffa5afa853af6d
Elvis Pranskevichus <elvis@geldata.com>
2025-07-31 15:17:43 -0700

    integration/macos: Fix test entrypoint

--- integration/macos/test.sh
+++ integration/macos/test.sh
@@ -9,30 +9,6 @@ fi
 if [ -n "${PKG_PLATFORM_VERSION}" ]; then
     dest="${dest}-${PKG_PLATFORM_VERSION}"
 fi
-if [ -n "${PKG_TEST_JOBS}" ]; then
-    dash_j="--jobs=${PKG_TEST_JOBS}"
-else
-    dash_j=""
-fi
-test_dir="${workdir}/share/tests"
-test_files="$test_dir"
-if [ -n "${PKG_TEST_FILES}" ]; then
-    # ${PKG_TEST_FILES} is specificaly used outside the quote so that it
-    # can contain a glob.
-    test_files=$(cd "$test_dir" && realpath $PKG_TEST_FILES)
-fi
-test_select=""
-if [ -n "${PKG_TEST_SELECT}" ]; then
-    for pattern in $PKG_TEST_SELECT; do
-      test_select="$test_select --include=${pattern}"
-    done
-fi
-test_exclude=""
-if [ -n "${PKG_TEST_EXCLUDE}" ]; then
-    for pattern in $PKG_TEST_EXCLUDE; do
-      test_exclude="$test_exclude --exclude=${pattern}"
-    done
-fi
 
 cliurl="https://packages.geldata.com/dist/${PKG_PLATFORM_VERSION}-apple-darwin"
 
@@ -74,6 +50,31 @@ if [ "$1" == "bash" ]; then
 fi
 
 export PATH="${workdir}/bin/:${PATH}"
+
+if [ -n "${PKG_TEST_JOBS}" ]; then
+    dash_j="--jobs=${PKG_TEST_JOBS}"
+else
+    dash_j=""
+fi
+test_dir="${workdir}/share/tests"
+test_files="$test_dir"
+if [ -n "${PKG_TEST_FILES}" ]; then
+    # ${PKG_TEST_FILES} is specificaly used outside the quote so that it
+    # can contain a glob.
+    test_files=$(cd "$test_dir" && realpath $PKG_TEST_FILES)
+fi
+test_select=""
+if [ -n "${PKG_TEST_SELECT}" ]; then
+    for pattern in $PKG_TEST_SELECT; do
+      test_select="$test_select --include=${pattern}"
+    done
+fi
+test_exclude=""
+if [ -n "${PKG_TEST_EXCLUDE}" ]; then
+    for pattern in $PKG_TEST_EXCLUDE; do
+      test_exclude="$test_exclude --exclude=${pattern}"
+    done
+fi
 
 "${workdir}/bin/python3" \
     -m edb.tools --no-devmode test \
